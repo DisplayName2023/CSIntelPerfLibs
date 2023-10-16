41f15704cfbcb980de26bfc6ea5b306151d5822e
Slash 2WD <wanwei@sinolink-technologies.com>
2023-10-16 17:47:07 +0800

    1.0.0.1

--- Tools/nuget_push.ps1
+++ Tools/nuget_push.ps1
@@ -6,6 +6,17 @@ Set-Location $scriptDir
 
 $nuget = "$scriptDir\Nuget.exe"
 
+$solutionDir = "$scriptDir\.."
+
+# Get the content of the version.cs file
+$content = Get-Content -Path "${solutionDir}\CSIntel.Ipp\Properties\AssemblyInfo.cs"
+# Find the line that contains the assembly version attribute
+$line = $content | Where-Object { $_ -match '\[assembly: AssemblyVersion\("(.*)"\)\]' }
+# Extract the version text from the line using a regular expression
+$version = $line -replace '\[assembly: AssemblyVersion\("(.*)"\)\]', '$1'
+# Print the version text
+Write-Output "Version: $version"
+
 
 $package = "$scriptDir\..\Package.nuspec"
 if ((Test-Path $package) -eq $true)
@@ -18,7 +29,7 @@ if ((Test-Path $package) -eq $true)
 
     $ippReleaseDir = "$scriptDir\..\CSIntel.Ipp\bin\Release"
     Remove-Item -Path $ippReleaseDir\*.nupkg -Force
-    & dotnet pack "$scriptDir\..\CSIntel.Ipp\CSIntel.Ipp.NET.csproj" --configuration=Release
+    & dotnet pack "$scriptDir\..\CSIntel.Ipp\CSIntel.Ipp.NET.csproj" --configuration Release -p:Version=$version
     $nupkgFile = Get-ChildItem -Path "$ippReleaseDir\*.nupkg"    
 }
 
